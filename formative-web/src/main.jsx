e5055dc13aa316123790be881ee9ec3de713ce78
Marc Carrillo <carrillomarc34@gmail.com>
2026-08-27 20:01:36 -0700

    Add Vercel Analytics and Speed Insights to Scroll Trivia.

--- formative-web/src/main.jsx
+++ formative-web/src/main.jsx
@@ -1,6 +1,8 @@
 import { Component, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import App from './App.jsx';
 import './theme/formative.css';
 
@@ -52,6 +54,8 @@ createRoot(document.getElementById('root')).render(
       <BrowserRouter basename="/trivia">
         <App />
       </BrowserRouter>
+      <Analytics />
+      <SpeedInsights />
     </TriviaErrorBoundary>
   </StrictMode>
 );
